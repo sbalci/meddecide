89d3530b19d365fd8ab84313e14cbc0652cb8508
SERDAR BALCI <drserdarbalci@gmail.com>
2020-08-05 19:41:59 +0300

    WIP at: 2020-08-05 19:41:59

---
+++ R/decision2.b.R
@@ -0,0 +1,13 @@
+
+# This file is a generated template, your changes will not be overwritten
+
+decision2Class <- if (requireNamespace('jmvcore')) R6::R6Class(
+    "decision2Class",
+    inherit = decision2Base,
+    private = list(
+        .run = function() {
+
+
+
+        })
+)
