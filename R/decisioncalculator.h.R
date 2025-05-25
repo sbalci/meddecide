d549f97d63cce54e43742ea074d75abec5456c3c
sbalci <drserdarbalci@gmail.com>
2025-05-25 09:51:35 +0300

    WIP

--- R/decisioncalculator.h.R
+++ R/decisioncalculator.h.R
@@ -331,7 +331,7 @@ decisioncalculatorBase <- if (requireNamespace("jmvcore", quietly=TRUE)) R6::R6C
             super$initialize(
                 package = "meddecide",
                 name = "decisioncalculator",
-                version = c(1,0,0),
+                version = c(0,0,3),
                 options = options,
                 results = decisioncalculatorResults$new(options=options),
                 data = data,
